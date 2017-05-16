0a700c3ec3c75a885ca29410ea7632281c06322c
chengler <christian.engler@gmx.de>
2017-05-16 11:20:16 +0200

    Eingabe abspielbarer Medien möglich, wenn editiren erlaubt

--- WebContent/venues/modalVenue.js
+++ WebContent/venues/modalVenue.js
@@ -22,6 +22,7 @@ angular.module('modalVenue', [ 'ui.bootstrap', 'ffkUtils' ]).constant('MODULE_VE
 		// TODO stack für asyncrone Serverantworten
 
 		// Die Antwort des ModalVenueInstanceCtrl
+		// speicher Änderungen
 		modalInstance.result.then(function(res) {
 			$log.debug("todo OpenModalVenueService: " + JSON.stringify(res, 1, 4));
 			
@@ -105,6 +106,52 @@ angular.module('modalVenue').controller('ModalVenueInstanceCtrl',
 			} else {
 				console.log("Spielort neu anlegen");
 			}
+
+            // setze Medienvariblen zur Verfügbarkeit
+			// alle Medientypen
+            $scope.medienTypen = ["BD","35mm","DCP","analog"];
+			// die abspielbaren Medientypen des Spielortes
+			var sidMedien = $rootScope.spielorte[sid].medien;
+			// alle Medien und ob sie vor Ort abspielbar sind
+			$scope.medium = {};
+
+            $scope.medienTypen.forEach(function(typ) {
+                console.log(typ);
+				$scope.medium[typ] = {};
+                $scope.medium[typ].typ = typ;
+                $scope.medium[typ].btn = "btn-danger";
+                $scope.medium[typ].set = false;
+                if(sidMedien.indexOf(typ) > -1){
+                    $scope.medium[typ].set = true;
+                    $scope.medium[typ].btn = "btn-success";
+                }
+                console.log($scope.medium[typ]);
+            });
+
+            $scope.toggleMedium = function(typ) {
+            	if ($scope.bearbeiten){
+
+                console.log("toogle "+typ);
+                if ($scope['medium'][typ].set) {
+                    $scope['medium'][typ].set = false;
+                    $scope['medium'][typ].btn = "btn-danger";
+                    // lösche String aus array
+                    var index = $scope.thisVenue.medien.indexOf(typ);    // <-- Not supported in <IE9
+                    if (index !== -1) {
+                        $scope.thisVenue.medien.splice(index, 1);
+                    }
+                } else {
+                    $scope['medium'][typ].set = true;
+                    $scope['medium'][typ].btn = "btn-success";
+                    $scope.thisVenue.medien.push(typ);
+;
+                }
+                }else {
+                    console.log("nicht im bearbeitungsmodus für toogle "+typ);
+				}
+            };
+
+
 
 			// Venue speichern
 			$scope.speichern = function() {
